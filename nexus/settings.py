29990dc8ea95fa4e3e333f92610fe10b067275d9
Palash Pingale <palashpingale135@gmail.com>
2026-04-10 17:20:21 +0530

    Fix deployment config

--- nexus/settings.py
+++ nexus/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
